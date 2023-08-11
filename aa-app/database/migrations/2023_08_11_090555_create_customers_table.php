3bec9e438ee97dd345a8df9f9386dba47b3ba925
Shepherd Yaw Morttey <shepherd@mfidie.com>
2023-08-11 09:40:51 +0000

    fixed error in customer table

--- aa-app/database/migrations/2023_08_11_090555_create_customers_table.php
+++ aa-app/database/migrations/2023_08_11_090555_create_customers_table.php
@@ -4,23 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDesignsTable extends Migration
+class CreateCustomersTable extends Migration
 {
     public function up()
     {
-        Schema::create('designs', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('customer_id');
-            $table->string('photo_path'); // Path to the stored photo
-            $table->text('description')->nullable(); // Optional description of the design
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
             $table->timestamps();
-
-            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('designs');
+        Schema::dropIfExists('customers');
     }
 }
