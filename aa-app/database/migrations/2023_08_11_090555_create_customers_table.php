be4bf5b2898fcee6429f1f17db7c8f12c724c7d5
Shepherd Yaw Morttey <shepherd@mfidie.com>
2023-08-11 09:35:32 +0000

    updated 3 main tables

--- aa-app/database/migrations/2023_08_11_090555_create_customers_table.php
+++ aa-app/database/migrations/2023_08_11_090555_create_customers_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateDesignsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('designs', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->string('photo_path'); // Path to the stored photo
+            $table->text('description')->nullable(); // Optional description of the design
             $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('designs');
     }
-};
+}
--- aa-app/database/migrations/2023_08_11_091734_create_measurements_table.php
+++ aa-app/database/migrations/2023_08_11_091734_create_measurements_table.php
@@ -4,24 +4,40 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateMeasurementsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('measurements', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->float('height')->nullable();
+            $table->float('weight')->nullable();
+            $table->float('bust')->nullable();
+            $table->float('waist')->nullable();
+            $table->float('hips')->nullable();
+            $table->float('back_waist_length')->nullable();
+            $table->float('front_waist_length')->nullable();
+            $table->float('shoulder_to_shoulder')->nullable();
+            $table->float('chest_depth')->nullable();
+            $table->float('armhole_depth')->nullable();
+            $table->float('inseam')->nullable();
+            $table->float('crotch_depth')->nullable();
+            $table->float('neck_circumference')->nullable();
+            $table->float('sleeve_length')->nullable();
+            $table->float('bicep_circumference')->nullable();
+            $table->float('forearm_circumference')->nullable();
+            $table->float('thigh_circumference')->nullable();
+            $table->float('knee_circumference')->nullable();
+            $table->float('calf_circumference')->nullable();
+            $table->float('ankle_circumference')->nullable();
             $table->timestamps();
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('measurements');
     }
-};
+}
