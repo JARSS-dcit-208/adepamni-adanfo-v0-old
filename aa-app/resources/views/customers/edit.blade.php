097cb92cb99f17583ff2f49ecd00edca967d6249
Shepherd Yaw Morttey <shepherd@mfidie.com>
2023-08-13 20:26:49 +0000

    fixed bug relating to editing customer

--- aa-app/resources/views/customers/edit.blade.php
+++ aa-app/resources/views/customers/edit.blade.php
@@ -2,22 +2,43 @@
 
 @section('content')
 <div class="container mx-auto mt-10 p-4 lg:w-1/2 md:w-3/4 sm:w-full">
-    <h1 class="text-2xl mb-6 font-semibold text-center">Edit Measurement for {{ $measurement->customer->name }}</h1>
-    
-    <form action="{{ route('measurements.update', $measurement->id) }}" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <h1 class="text-2xl mb-6 font-semibold text-center">Edit Customer</h1>
+    <form action="{{ route('customers.update', $customer->id) }}" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         @csrf
         @method('PUT')
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                Name:
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" name="name" value="{{ $customer->name }}" required>
+        </div>
 
-        <input type="hidden" name="customer_id" value="{{ $measurement->customer_id }}">
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                Email:
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" name="email" value="{{ $customer->email }}" required>
+        </div>
 
-        
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="phone">
+                Phone:
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="phone" type="text" name="phone" value="{{ $customer->phone }}" required>
+        </div>
 
-        <div class="flex items-center justify-center mt-6">
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Update Measurement</button>
+        <div class="mb-6">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="address">
+                Address:
+            </label>
+            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="address" name="address" rows="4" required>{{ $customer->address }}</textarea>
+        </div>
+
+        <div class="flex items-center justify-center">
+            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                Update
+            </button>
         </div>
     </form>
-    <div class="text-center mt-4">
-        <a href="{{ route('measurements.index') }}" class="text-blue-500 hover:underline">Back to Measurements List</a>
-    </div>
 </div>
 @endsection
