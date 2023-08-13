1a149ebcbc5cb305deea41e64a4e7e2369a9009a
Shepherd Yaw Morttey <shepherd@mfidie.com>
2023-08-13 01:30:42 +0000

    set up endpoints for CRUD operations

--- aa-app/routes/api.php
+++ aa-app/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\DesignController;
+use App\Http\Controllers\MeasurementController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Customers API Routes
+Route::apiResource('customers', CustomerController::class);
+
+// Designs API Routes
+Route::apiResource('designs', DesignController::class);
+
+// Measurements API Routes
+Route::apiResource('measurements', MeasurementController::class);
